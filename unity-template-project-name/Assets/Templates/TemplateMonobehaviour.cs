108edf424f9281869d7f16435906025844ae607a
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-21 19:16:15 +0200

    Add templates

---
+++ unity-template-project-name/Assets/Templates/TemplateMonobehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Project.Sub {
+    public class TemplateMonobehaviour : MonoBehaviour {
+	
+	    #region cache
+		private Transform m_Transform;
+		#endregion
+
+        void Awake(){
+			m_Transform = this.transform;
+        }
+    }
+}
