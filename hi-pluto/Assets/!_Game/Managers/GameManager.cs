abe7a783816e3cd4ca1842f60fa45798db1d1b10
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-22 11:40:21 +0200

    no message

--- hi-pluto/Assets/!_Game/Managers/GameManager.cs
+++ hi-pluto/Assets/!_Game/Managers/GameManager.cs
@@ -6,12 +6,38 @@ namespace Lime.LudumDare.HiPluto.Managers {
 		[SerializeField]
 		private bool m_VisibleCursor;
 
+		[SerializeField]
+		private Transform m_Player;
 
+		#region cache
+		private Rigidbody m_PlayerRigidBody;
+	
+
+		#endregion endCache
 		private void Start() {
+			if (m_Player == null) {
+				Debug.LogWarning("Player isn't set! Finding player");
+				GameObject playerGo = GameObject.FindGameObjectWithTag("Player");
+				if (playerGo == null) {
+					Debug.LogError("No GO with Player tag found! Abort abort!");
+#if UNITY_EDITOR
+					UnityEditor.EditorApplication.isPlaying = false;
+#endif
+				}
+				else {
+					m_Player = playerGo.transform;
+				}
+			}
 			Setup();
+			Cache();
 		}
 		private void Setup(){
 			Cursor.visible = m_VisibleCursor;
 		}
+
+		private void Cache() {
+			//m_PlayerRigidBody = 
+		}
+
     }
 }
---
+++ hi-pluto/Assets/!_Game/Script Components/PushPlayerUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Lime.LudumDare.HiPluto.Components.Physics {
+    public class PushPlayerUp : MonoBehaviour {
+	
+	    #region cache
+		private Rigidbody m_Rigidbody;
+		#endregion
+
+        void Awake(){
+			m_Rigidbody = this.GetComponent<Rigidbody>();
+        }
+
+
+    }
+}
