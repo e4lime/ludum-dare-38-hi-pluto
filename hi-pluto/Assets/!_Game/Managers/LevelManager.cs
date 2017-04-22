9d17051e50306f23411186a7597562a657d22203
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-22 19:25:36 +0200

    Simple level

--- hi-pluto/Assets/!_Game/Managers/LevelManager.cs
+++ hi-pluto/Assets/!_Game/Managers/LevelManager.cs
@@ -4,18 +4,19 @@ namespace Lime.LudumDare.HiPluto.Managers {
     public class LevelManager : MonoBehaviour {
 
 
-		[SerializeField, Header("Max distance between two jumpObjects")]
-		private float m_JumpObjectDistance;
+		[SerializeField, Header("Max distance between two jumpObjects, should be tweaked with jumps height")]
+		private float m_JumpObjectDistance = 3.8f;
+		[SerializeField]
+		private float m_MinJumpObjectDistance = 1f;
 
 		[SerializeField]
 		private Checkpoint[] m_Checkpoints;
-
+		[SerializeField]
+		private GameObject[] m_JumpObjects;
 		[SerializeField, Header("Level Size")]
 		private Transform m_LeftEndOfLevel;
-
 		[SerializeField]
 		private Transform m_RightEndOfLevel;
-
 		[SerializeField]
 		private Transform m_BottomOfLevel;
 
@@ -26,6 +27,24 @@ namespace Lime.LudumDare.HiPluto.Managers {
 		}
 
 		private void CreateLevel() {
+
+			float currentHeight = m_BottomOfLevel.transform.position.y;
+			
+			while (currentHeight < 1000f) {
+				float nextY = Random.Range(m_MinJumpObjectDistance, m_JumpObjectDistance);
+				currentHeight += nextY;
+				GameObject jumpObj = GameObject.Instantiate(m_JumpObjects[0],transform);
+				Vector3 newPos = new Vector3(GetRandomX(), currentHeight, m_BottomOfLevel.position.z);
+				jumpObj.transform.position = newPos;
+				m_LatestCreatedJumpObjectLocation = newPos;
+				Debug.Log("create");
+			}
+
+		}
+
+		private float GetRandomX() {
+			//return m_BottomOfLevel.position.x;
+			return Random.Range(m_LeftEndOfLevel.position.x, m_RightEndOfLevel.position.x);
 
 		}
 
