7a82bb37f53b0a2c522d0c98997aa571586dbc4c
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-22 13:11:09 +0200

    Bounce ball with tween

--- hi-pluto/Assets/!_Game/Script Components/PushPlayer.cs
+++ hi-pluto/Assets/!_Game/Script Components/PushPlayer.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 namespace Lime.LudumDare.HiPluto.Components.Physics {
     public class PushPlayer : MonoBehaviour {
@@ -6,16 +7,12 @@ namespace Lime.LudumDare.HiPluto.Components.Physics {
 		[SerializeField]
 		private Rigidbody m_PlayerRigidbody;
 
+		[SerializeField, Range(0, 4)]
+		private float m_Height;
+		[SerializeField, Range(0, 4)]
+		private float m_Duration;
 		[SerializeField]
-		private Vector3 m_PushDirection;
-
-		#region cache
-		private Rigidbody m_Rigidbody;
-		#endregion
-
-        void Awake(){
-			m_Rigidbody = this.GetComponent<Rigidbody>();
-        }
+		private Ease m_Ease;
 
 		void Start() {
 			if (m_PlayerRigidbody == null) {
@@ -24,10 +21,24 @@ namespace Lime.LudumDare.HiPluto.Components.Physics {
 		}
 
 		private void OnTriggerEnter() {
-			Vector3 currentVel = m_PlayerRigidbody.velocity;
-			m_PlayerRigidbody.velocity = new Vector3(currentVel.x, 7, currentVel.z);
+
+	
+			Vector3 currentPos = m_PlayerRigidbody.position;
+			m_PlayerRigidbody.DOMoveY(currentPos.y + m_Height, m_Duration).SetLoops(2, LoopType.Yoyo).SetEase(m_Ease);
+			//m_PlayerRigidbody.DOJump(currentPos, 3, 2, 2);
+
+			//	Vector3 currentVel = m_PlayerRigidbody.velocity;
+			//	m_PlayerRigidbody.velocity = new Vector3(currentVel.x, 7, currentVel.z);
 			//m_PlayerRigidbody.AddForce(m_PushDirection,ForceMode.Impulse);
 		}
 
+
+		private void TweenBackDown() {
+			//m_PlayerRigidbody.DOMoveY(-3, 2).OnComplete(TweenBackDown);
+			//m_PlayerRigidbody.velocity = new Vector3(m_PlayerRigidbody.velocity.x, 0, m_PlayerRigidbody.velocity.z);
+
+
+
+		}
 	}
 }
---
+++ hi-pluto/Assets/!_Game/Script Components/Tweens/JumpTween.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace Lime.LudumDare.HiPluto.Tweens {
+    public class Tweens : MonoBehaviour {
+	
+	    #region cache
+		private Rigidbody m_Rigidbody;
+		#endregion
+
+        void Awake(){
+			m_Rigidbody = this.GetComponent<Rigidbody>();
+			Vector3 currentPos = m_Rigidbody.position;
+			m_Rigidbody.DOJump(currentPos,10, 1, 5);
+        }
+
+    }
+}
