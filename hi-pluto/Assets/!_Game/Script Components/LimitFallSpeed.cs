9b168b9a925f0b12ef231f3f39ff1298500e112c
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-22 15:13:54 +0200

    Limit fallspeed

---
+++ hi-pluto/Assets/!_Game/Script Components/LimitFallSpeed.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Lime.LudumDare.HiPluto {
+    public class LimitFallSpeed : MonoBehaviour {
+
+		[SerializeField, Range(-100, 0)]
+		private float m_LimitAtSpeed = -11f;
+
+	    #region cache
+		private Rigidbody m_Rigidbody;
+		#endregion
+
+	
+        void Awake(){
+			m_Rigidbody = GetComponent<Rigidbody>();
+        }
+
+
+
+		private void FixedUpdate() {
+			Vector3 cv = m_Rigidbody.velocity;
+			if (cv.y < m_LimitAtSpeed) {
+				m_Rigidbody.velocity = new Vector3(cv.x, m_LimitAtSpeed, cv.z);
+			}
+		}
+	}
+}
