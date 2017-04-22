84839c71fc0146b48c0dc23f9da7be7d8232c322
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-22 14:59:22 +0200

    Horizontal camera. Add smooth camera follow

---
+++ hi-pluto/Assets/!_Game/Script Components/SmoothFollowTarget.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Lime.LudumDare.HiPluto.Components {
+    public class SmoothFollowTarget : MonoBehaviour {
+
+		[SerializeField]
+		private Rigidbody m_Target;
+
+		[SerializeField]
+		private float m_Damp = 0.3f;
+		#region cache
+		private Transform m_Transform;
+		#endregion
+
+
+		private Vector3 velocity = Vector3.zero;
+        void Awake(){
+			m_Transform = this.transform;
+		}
+
+		void FixedUpdate() {
+			Vector3 targetPos = new Vector3(m_Transform.position.x, m_Target.position.y, m_Transform.position.z);
+			m_Transform.position = Vector3.SmoothDamp(m_Transform.position, targetPos, ref velocity, m_Damp);
+		}
+	}
+}
