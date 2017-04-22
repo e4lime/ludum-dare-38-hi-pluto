6576f28aa132641a11cc579b0db6b240e93538d4
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-22 12:36:54 +0200

    Bouncy ball

--- hi-pluto/Assets/!_Game/Script Components/PlayerController.cs
+++ hi-pluto/Assets/!_Game/Script Components/PlayerController.cs
@@ -3,18 +3,19 @@
 namespace Lime.LudumDare.HiPluto {
     public class PlayerController : MonoBehaviour {
 	
-	    #region cache
-		private Rigidbody m_Rigidbody;
-		#endregion
+	    [SerializeField]
+		private Rigidbody m_PlayerRigidbody;
+		
 
         void Awake(){
-			m_Rigidbody = this.GetComponent<Rigidbody>();
+			if (m_PlayerRigidbody == null)
+				m_PlayerRigidbody = this.GetComponent<Rigidbody>();
         }
 
 		void Update(){
 			Vector3 mousePos = Input.mousePosition;
 			float desiredX = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, 0, 10)).x;
-			m_Rigidbody.position = new Vector3(desiredX, m_Rigidbody.position.y, m_Rigidbody.position.z);
+			m_PlayerRigidbody.position = new Vector3(desiredX, m_PlayerRigidbody.position.y, m_PlayerRigidbody.position.z);
 		}
 	}
 }
---
+++ hi-pluto/Assets/!_Game/Script Components/PushPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Lime.LudumDare.HiPluto.Components.Physics {
+    public class PushPlayer : MonoBehaviour {
+
+		[SerializeField]
+		private Rigidbody m_PlayerRigidbody;
+
+		[SerializeField]
+		private Vector3 m_PushDirection;
+
+		#region cache
+		private Rigidbody m_Rigidbody;
+		#endregion
+
+        void Awake(){
+			m_Rigidbody = this.GetComponent<Rigidbody>();
+        }
+
+		void Start() {
+			if (m_PlayerRigidbody == null) {
+				Debug.LogError("Player is null!");
+			}
+		}
+
+		private void OnTriggerEnter() {
+			Vector3 currentVel = m_PlayerRigidbody.velocity;
+			m_PlayerRigidbody.velocity = new Vector3(currentVel.x, 7, currentVel.z);
+			//m_PlayerRigidbody.AddForce(m_PushDirection,ForceMode.Impulse);
+		}
+
+	}
+}
