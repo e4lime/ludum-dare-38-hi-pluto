fca3d20f8ee2d985224bdb007c5bf5e0444744c4
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-22 11:19:44 +0200

    no message

---
+++ hi-pluto/Assets/!_Game/Managers/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Lime.LudumDare.HiPluto.Managers {
+    public class GameManager : MonoBehaviour {
+	
+		[SerializeField]
+		private bool m_VisibleCursor;
+
+
+		private void Start() {
+			Setup();
+		}
+		private void Setup(){
+			Cursor.visible = m_VisibleCursor;
+		}
+    }
+}
---
+++ hi-pluto/Assets/!_Game/Script Components/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Lime.LudumDare.HiPluto {
+    public class PlayerController : MonoBehaviour {
+	
+	    #region cache
+		private Rigidbody m_Rigidbody;
+		#endregion
+
+        void Awake(){
+			m_Rigidbody = this.GetComponent<Rigidbody>();
+        }
+
+		void Update(){
+			Vector3 mousePos = Input.mousePosition;
+			float desiredX = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, 0, 10)).x;
+			m_Rigidbody.position = new Vector3(desiredX, m_Rigidbody.position.y, m_Rigidbody.position.z);
+		}
+	}
+}
