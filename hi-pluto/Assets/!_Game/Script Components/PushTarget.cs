9c6bcf9fe1cc87d9fe4a855363ab7e5a4746e467
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-22 14:23:13 +0200

    Move pushlogic to pushable

---
+++ hi-pluto/Assets/!_Game/Script Components/PushTarget.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+namespace Lime.LudumDare.HiPluto.Components.Physics {
+	public class PushTarget : MonoBehaviour {
+
+
+		private void OnTriggerEnter(Collider other) {
+			Pushable target = other.gameObject.GetComponent<Pushable>();
+			if (target != null) {
+				target.Push();
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
---
+++ hi-pluto/Assets/!_Game/Script Components/Pushable.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace Lime.LudumDare.HiPluto.Components {
+
+	/// <summary>
+	/// Pushes the rigidbody in y axis
+	/// </summary>
+    public class Pushable : MonoBehaviour {
+	
+	 
+		[SerializeField]
+		private Rigidbody m_RigidBody;
+
+		[SerializeField, Range(0, 4)]
+		private float m_Height;
+		[SerializeField, Range(0, 4)]
+		private float m_Duration;
+		[SerializeField]
+		private Ease m_Ease;
+
+		void Awake(){
+			if (m_RigidBody == null) {
+				m_RigidBody = this.GetComponent<Rigidbody>();
+			}
+        }
+
+		public void Push() {
+			m_RigidBody.isKinematic = true;
+			m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, 0, m_RigidBody.velocity.z);
+			Vector3 currentPos = m_RigidBody.position;
+			m_RigidBody.DOMoveY(currentPos.y + m_Height, m_Duration)
+				.SetLoops(1, LoopType.Yoyo)
+				.SetEase(m_Ease)
+				.OnComplete(OnCompleteCallback);
+		}
+
+		private void OnCompleteCallback() {
+			m_RigidBody.isKinematic = false;
+		}
+    }
+}
