4b2881021160f31ce103edfb6c6d99ed7515e7ff
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-22 18:28:01 +0200

    no message

---
+++ hi-pluto/Assets/!_Game/Script Entities/Celestial Objects/CelestialObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Lime.LudumDare.HiPluto.Entities.CelestialObjects {
+    public class CelestialObject : MonoBehaviour {
+
+		[SerializeField, Header("Size modifier compared to earth")]
+		private string m_Name;
+		private string m_SizeModifier;
+    }
+}
