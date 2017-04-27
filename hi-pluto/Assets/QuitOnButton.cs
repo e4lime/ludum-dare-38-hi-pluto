a4d0f674df26defb3e368f8f65935736b426d00c
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-27 14:03:42 +0200

    Add quit on button

---
+++ hi-pluto/Assets/QuitOnButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Lime.LudumDare.HiPluto {
+	public class QuitOnButton : MonoBehaviour {
+
+
+		[SerializeField]
+		private string m_ButtonName;
+
+		private void Update() {
+			if (Input.GetButtonDown(m_ButtonName)) {
+				Application.Quit();
+			}
+		}
+	}
+}
