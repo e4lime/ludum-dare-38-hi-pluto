1c965a51f0cc155acbcf2c9f182cd7e88c3e17d1
Emil Lindberg <emil.lindberg86@gmail.com>
2017-04-27 14:30:48 +0200

    Add Quit button

---
+++ hi-pluto/Assets/!_Game/GUI/QuitApplication.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Lime.LudumDare.HiPluto {
+    public class QuitApplication : MonoBehaviour {
+	
+	   public void DoQuitApplication() {
+			Application.Quit();
+		}
+    }
+}
