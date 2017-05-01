070e51a92d8b4af6ce38a0a17ffccedacc3783ac
Emil Lindberg <emil.lindberg86@gmail.com>
2017-05-01 20:12:13 +0200

    Fix quality

---
+++ hi-pluto/Assets/!_Game/Script Components/DestroyOnWebGL.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Lime.LudumDare.HiPluto {
+    public class DestroyOnWebGL : MonoBehaviour {
+
+
+
+#if UNITY_WEBGL
+		void LateUpdate(){
+			Destroy(this.gameObject);
+        }
+    }
+#endif
+}
