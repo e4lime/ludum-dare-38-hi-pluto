f41d05b215419c4adf7b9e7e6df2d33029cd8be3
Emil Lindberg <emil.lindberg86@gmail.com>
2018-03-13 14:17:48 +0100

    Add mobile quality change physics timestep change instructions based on build target

---
+++ hi-pluto/Assets/AppendTextBasedOnMobileOrStandalone.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace E4lime.LudumDare.Ld38 {
+	public class AppendTextBasedOnMobileOrStandalone : MonoBehaviour {
+		
+		[SerializeField]
+		private Text m_Text = null;
+
+#if UNITY_STANDALONE || UNITY_EDITOR
+		[SerializeField]
+		private string[] m_StandaloneTextLines;
+#endif
+
+#if UNITY_ANDROID
+		[SerializeField]
+		private string[] m_AndroidTextLines;
+#endif
+		private string[] m_TextLinesToUse;
+
+		void Start() {
+			
+#if UNITY_STANDALONE || UNITY_EDITOR
+			m_TextLinesToUse = m_StandaloneTextLines;
+#endif
+
+#if UNITY_ANDROID
+			m_TextLinesToUse = m_AndroidTextLines;
+#endif
+
+			string newLine = System.Environment.NewLine;
+
+			m_Text.text += newLine + newLine;
+			foreach (string s in m_TextLinesToUse) {
+				m_Text.text += s;
+				m_Text.text += System.Environment.NewLine;
+			}
+		}
+	}
+}
+
---
+++ hi-pluto/Assets/PlayerSettings.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace E4lime.LudumDare.Ld38 {
+	public class PlayerSettings : MonoBehaviour {
+
+
+#if UNITY_ANDROID
+		void Awake() {
+
+
+			Application.targetFrameRate = 60;
+			QualitySettings.vSyncCount = 0;
+			QualitySettings.antiAliasing = 0;
+
+			QualitySettings.shadowCascades = 0;
+			QualitySettings.shadowDistance = 15;
+
+			Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+
+		}
+
+#endif
+	}
+}
